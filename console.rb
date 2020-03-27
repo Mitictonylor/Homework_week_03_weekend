951bd0c36825eb505e957cc13dfee4771ba9fde7
Antonio Lorusso <mitictonylor@gmail.com>
2020-03-27 17:02:31 +0000

    fill up console.rb with data

--- console.rb
+++ console.rb
@@ -26,7 +26,56 @@ customer4 = Customer.new({'name' => 'Keira',
 customer4.save();
 
 
+film1 = Film.new({'title' => "Avengers",
+                  'price' => 8.00
+                })
+film1.save()
 
+film2 = Film.new({'title' => "Hulk",
+                  'price' => 7.00
+                  })
+film2.save()
+
+screen1 = Screening.new({'show_time' => '18:30',
+                          'ticket_available' => 100
+                        })
+screen1.save()
+
+screen2 = Screening.new({'show_time' => '15:30',
+                          'ticket_available' => 200
+                        })
+screen2.save()
+#antonio, bought a ticket for avengers show at 18:30
+ticket1 = Ticket.new({'customer_id' => customer1.id,
+                      'film_id' => film1.id,
+                      'screening_id' => screen1.id
+                      })
+ticket1.save()
+
+
+ticket2 = Ticket.new({'customer_id' => customer2.id,
+                      'film_id' => film1.id,
+                      'screening_id' => screen1.id
+                      })
+ticket2.save()
+
+ticket3 = Ticket.new({'customer_id' => customer3.id,
+                      'film_id' => film1.id,
+                      'screening_id' => screen1.id
+                      })
+ticket3.save()
+
+ticket4 = Ticket.new({'customer_id' => customer4.id,
+                      'film_id' => film2.id,
+                      'screening_id' => screen2.id
+                      })
+ticket4.save()
+
+ticket5 = Ticket.new({'customer_id' => customer1.id,
+                      'film_id' => film2.id,
+                      'screening_id' => screen2.id
+                      })
+ticket5.save()
 
 
 
